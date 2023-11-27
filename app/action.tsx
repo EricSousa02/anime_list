53db0a51b83701130fdd21177b22a63ad021b643
EricSousa02 <ericsousadeandrade@gmail.com>
2023-11-27 16:58:38 -0300

    height fix

--- app/action.tsx
+++ app/action.tsx
@@ -1,17 +1,21 @@
-"use server";
-
+import axios from 'axios';
 import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
 
 const MAX_LIMIT = 8;
 
 export async function fetchAnime(page: number) {
-  const response = await fetch(
-    `https://shikimori.one/api/animes?page=${page}&limit=${MAX_LIMIT}&order=popularity`
-  );
+  try {
+    const response = await axios.get(
+      `https://shikimori.one/api/animes?page=${page}&limit=${MAX_LIMIT}&order=popularity`
+    );
 
-  const data = await response.json();
+    const data: AnimeProp[] = response.data;
 
-  return data.map((anime: AnimeProp, index: number) => (
-    <AnimeCard key={anime.id} anime={anime} index={index} />
-  ));
+    return data.map((anime: AnimeProp, index: number) => (
+      <AnimeCard key={anime.id} anime={anime} index={index} />
+    ));
+  } catch (error) {
+    console.error('Erro ao buscar animes:', error);
+    throw error; 
+  }
 }
